23563752971a88267927e276edc0c96f20bb7f86
thejerome <thejerome@mail.ru>
2020-07-15 00:22:13 +0300

    Repository.branches() is removed

--- src/main/java/com/epam/rd/autocode/observer/git/Repository.java
+++ src/main/java/com/epam/rd/autocode/observer/git/Repository.java
@@ -1,11 +1,7 @@
 package com.epam.rd.autocode.observer.git;
 
-import java.util.List;
-
 public interface Repository {
     void addWebHook(WebHook webHook);
-
-    List<String> branches();
 
     Commit commit(String branch, String author, String[] changes);
 
